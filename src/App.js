738ea1c6593e58835c4537fcd8b804c7d544f7c5
Qreamville <Oyeleyebalikis@yahoo.com>
2022-11-03 01:42:16 -1200

    feat: Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import { Contact, Error, Home } from "./pages";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/utils/links.js
@@ -0,0 +1,45 @@
+export const links = [
+  {
+    id: 1,
+    tag: "twitter",
+    name: "Twitter Link",
+    link: "https://twitter.com/Balqees_Oyeleye",
+  },
+  {
+    id: 2,
+    tag: "btn__zuri",
+    name: "Zuri Team",
+    link: "https://training.zuri.team/",
+  },
+  {
+    id: 3,
+    tag: "books",
+    name: "Zuri Books: This is where you find books about design and coding",
+    link: "https://books.zuri.team/",
+  },
+  {
+    id: 4,
+    tag: "book__python",
+    name: "Python Books: Featured book-Python for Beginners and get royalties of any purchase",
+    link: "https://books.zuri.team/python-for-beginners?ref_id=BalikisOyelaye",
+  },
+  {
+    id: 5,
+    tag: "pitch",
+    name: "Background Check for Coders: Hire for credible background checks",
+    link: "https://background.zuri.team/",
+  },
+  {
+    id: 6,
+    tag: "book__design",
+    name: "Design Books: Get free design books from Zuri",
+    link: "https://books.zuri.team/design-rules",
+  },
+  {
+    id: 7,
+    tag: "contact",
+    name: "Contact Me",
+    link: "/contact",
+    isPage: true,
+  },
+];
